e6b9bb9b99e556a65350e908999d3cb6a1a833d1
andersnauman <anders.nauman@gmail.com>
2019-05-16 19:49:34 +0200

    Improved in so many ways. Vulkan and SDL now compiles from source instead of pre-compiled

--- README.md
+++ README.md
@@ -4,6 +4,13 @@
 cd external
 hg clone https://hg.libsdl.org/SDL
 hg clone https://hg.libsdl.org/SDL_image
+git clone https://github.com/KhronosGroup/Vulkan-Headers.git
+cd Vulkan-Headers
+git checkout sdk-1.1.106
+
+git clone https://github.com/KhronosGroup/Vulkan-Loader.git
+cd Vulkan-Loader
+git checkout sdk-1.1.106
 ```
 
 ```
@@ -17,8 +24,19 @@ make install
 make
 make install
 
-# Windows - Cross-Compile
+# Windows - Cross-Compile from linux
+## Vulkan
+dnf install mingw64-vulkan-tools mingw64-vulkan-loader mingw64-vulkan-headers mingw64-vulkan-validation-layers
+## Other
+dnf install mingw64-zlib
 cmake -DCMAKE_TOOLCHAIN_FILE=../windows-toolchain.cmake ..
 make
 make install
 ```
+
+## Notes
+### Vulkan
+- FindVulkan.cmake cannot find mingw64 vulkan from dnf. Path's must be in lower case e.g $ENV{VULKAN_SDK}/Lib" -> $ENV{VULKAN_SDK}/lib"
+- Vulkan-Headers and Vulkan-Loader must be the same release/branch. Master on both will most likly yield wierd result.
+- Vulkan-Loader + MinGW64: WinSock2.h -> winsock2.h, Cfgmgr32.h -> cfgmgr32.h
+- Vulkan-Loader: asm_offset.exe must run to generate gen_defines.asm, a.k.a cannot cross-compile Vulkan-Loader from source code.
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
-#include "SDL.h"
+#include <vector>
 #include <iostream>
+
+#include "SDL.h"
+
+#include <vulkan/vulkan.h>
 
 #ifdef __cplusplus
 extern "C"
@@ -15,6 +19,42 @@ int main(int argc, char *argv[]) {
     if (window == NULL) {
         printf("Could not create window: %s\n", SDL_GetError());
         return 1;
+    }
+
+    VkInstance instance;
+
+    VkApplicationInfo appInfo = {};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create instance!");
+    }
+    uint32_t gpu_count = 0;
+    vkEnumeratePhysicalDevices(instance, &gpu_count, nullptr);
+    std::vector<VkPhysicalDevice> gpu_list(gpu_count);
+    vkEnumeratePhysicalDevices(instance, &gpu_count, gpu_list.data());
+    VkPhysicalDeviceProperties prop;
+    vkGetPhysicalDeviceProperties(gpu_list[0], &prop);
+    printf("Graphics: %s\n", prop.deviceName);
+
+    uint32_t family_count = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(gpu_list[0], &family_count, nullptr);
+    std::vector<VkQueueFamilyProperties> family_list(family_count);
+    vkGetPhysicalDeviceQueueFamilyProperties(gpu_list[0], &family_count, family_list.data());
+
+    for(auto f : family_list) {
+        if(f.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+            printf("Found graphics!!\n");
+        }
     }
 
     SDL_Delay(3000);
